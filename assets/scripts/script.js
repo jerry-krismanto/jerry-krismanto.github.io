5a493820d630107082c91af6a6d553d5a272fddd
Jerry Yan Krismanto <jerryykrismanto@gmail.com>
2021-12-01 22:27:34 +0700

    commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type", {
+  strings: ["Jerry Yan Krismanto", " a Front End Developer", "an Aspiring Programmer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
