95c951ff11d48f2cd67e9985693bdd6dd87c16ce
Jerry Yan Krismanto <jerryykrismanto@gmail.com>
2022-10-26 10:18:02 +0700

    reworked the whole page css using tailwind

--- assets/scripts/script.js
+++
@@ -1,6 +0,0 @@
-var typed = new Typed(".auto-type", {
-  strings: ["Jerry Yan Krismanto", " a Front End Developer", "an Aspiring Programmer"],
-  typeSpeed: 100,
-  backSpeed: 100,
-  loop: true,
-});
