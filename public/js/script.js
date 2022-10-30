0b139a33dca912f9af20005a3d7f52c4b82af53c
Jerry Yan Krismanto <jerryykrismanto@gmail.com>
2022-10-30 09:56:55 +0700

    fixed navbar

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,15 @@
+let menu = document.querySelector('#drop-down');
+// if(menu.style.width == 'auto'){
+//     menu.style.display = 'flex';
+// } else{
+//     menu.style.display = 'none';
+// }
+
 function hamburgerMenu(){
     let menu = document.querySelector('#drop-down');
-    if (menu.style.display == 'none'){
-        menu.style.display = 'flex';
-    } else {
+    if (menu.style.display == 'flex'){
         menu.style.display = 'none';
+    } else {
+        menu.style.display = 'flex';
     }
 }
